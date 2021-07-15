5ab6aad0259ebd5756dc526204fe02a696dd0eb4
joaoreis portatil <web@joaoreis.pt>
2021-07-15 21:24:04 +0100

    update autoload

--- test.php
+++ test.php
@@ -1,23 +1,40 @@
 <?php
 
+use joaoreisweb\NumbersInWords;
 
-//include 'src/numbersinwords.php';
-use NumbersInWords\NumbersInWords;
-
-
+require_once realpath("vendor/autoload.php");
 
 $number_convert = new NumbersInWords();
 
+$valor = '1114.55';
+echo "<br><br>numbers PT - ".$number_convert->formatNumber($valor) ."<br>";
+print_r( $number_convert->numbersInWords($valor,'PT'));
+
+$valor = '1114.55';
+echo "<br><br>numbers BR - ".$number_convert->formatNumber($valor) ."<br>";
+print_r( $number_convert->numbersInWords($valor,'BR'));
 
 $valor = '1116.55';
+echo "<br><br>numbers EN - ".$number_convert->formatNumber($valor) ."<br>";
+print_r( $number_convert->numbersInWords($valor,'EN'));
+
+echo '<br><br><hr>';
+
+$valor = '2000.55';
 echo "<br><br>money BR EUR - ".$number_convert->formatNumber($valor,'€') ."<br>";
 print_r( $number_convert->moneyInWords($valor,'BR','EUR'));
 
-$valor = '1116.55';
+$valor = '2000.55';
+echo "<br><br>money EN USD - ".$number_convert->formatNumber($valor,'€', 2, ' ','left') ."<br>";
+print_r( $number_convert->moneyInWords($valor,'EN','USD'));
+
+echo '<br><br><hr>';
+
+$valor = '1114.55';
 echo "<br><br>money PT EUR - ".$number_convert->formatNumber($valor,'€') ."<br>";
 print_r( $number_convert->moneyInWords($valor,'PT','EUR'));
 
+$valor = '1114.55';
+echo "<br><br>money BR EUR - ".$number_convert->formatNumber($valor,'€') ."<br>";
+print_r( $number_convert->moneyInWords($valor,'BR','EUR'));
 
-$valor = '1116.55';
-echo "<br><br>money EN USD - ".$number_convert->formatNumber($valor,'€') ."<br>";
-print_r( $number_convert->moneyInWords($valor,'EN','USD'));
